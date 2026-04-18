2a178a45a35607fdd581ddee9b0fbf9cd56e045e
Savageroyale <olufranklyn@gmail.com>
2026-04-18 15:38:11 +0100

    Remove Intro.js file and its contents

--- Intro.js
+++ Intro.js
@@ -1,6 +1,1 @@
-const name = 'Franklyn';
-const email = 'olufranklyn@gmail.com';
-const time = new Date('2026-04-18T00:00:00Z').toISOString(); 
-const PORT = 3000
-   
-console.log(`${name} - ${email} - ${time}`);
+
